1abc8ce8d9130bff4c9411affb876fe534120b6a
Grigorii Matiukhin <grigory36m@gmail.com>
2022-11-06 20:35:08 +0300

    Basic options

---
+++ lua/user/options.lua
@@ -0,0 +1,14 @@
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.cursorline = false
+o.undofile = true
+o.smartindent = true
+o.clipboard = "unnamedplus"
+o.splitright = true
+o.splitbelow = true
+o.signcolumn = "yes"
+o.scrolloff = 8
+o.sidescrolloff = 8
+o.updatetime = 300
