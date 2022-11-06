40608086223aba67f4545edd9470a1d2c424dbb1
Grigorii Matiukhin <grigory36m@gmail.com>
2022-11-06 21:21:58 +0300

    Custom keymaps

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,71 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Netrw Directory Listing on the left
+-- We will hijack it later to use `NvimTree`
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Insert --
+-- Press `jj` fast to exit to normal mode
+keymap("i", "jj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode when changing indentation of selected text
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("v", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("v", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("v", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Make Neovim keep previously yanked value after pasting
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+-- It looks like remappings from Visual mode also work in Visual Block mode
+-- keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+-- keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+-- keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+-- keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
