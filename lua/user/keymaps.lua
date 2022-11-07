1f866afd3801bf9452bea77093f07ac3a5529b51
Grigorii Matiukhin <grigory36m@gmail.com>
2022-11-07 23:12:04 +0300

    Telescope fuzzy finder

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -69,3 +69,8 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Telescope
+keymap("n", "<leader>ff", ":Telescope find_files<cr>", opts)
+-- keymap("n", "<leader>ff", ":lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
+keymap("n", "<C-t>", ":Telescope live_grep<cr>", opts)
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -63,6 +63,9 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+
 	-- Automatically set up config aufter cloning packer.nvim
 	if PACKER_BOOTSTRAP then
 		require("packer").sync()
