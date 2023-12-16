c4fdfd86a30af1f11e2e1bd1c0043b2ddd01c880
Grigorii Matiukhin <contact@gmatiukhin.site>
2023-12-16 15:21:24 +0300

    ui fix

--- lua/user/ui/init.lua
+++ lua/user/ui/init.lua
@@ -1,7 +1,4 @@
 require("user.ui.colorscheme") -- Important to load first
-
-if IsWorkstation then
-  require("user.ui.statusline")
-  require("user.ui.winbar")
-  require("user.ui.bufferline")
-end
+require("user.ui.statusline")
+require("user.ui.winbar")
+require("user.ui.bufferline")
