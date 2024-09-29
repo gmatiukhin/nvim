a85fe154ff166e9b057071dfa01fdd51db35e637
Grigorii Matiukhin <contact@gmatiukhin.site>
2024-09-29 17:57:34 +0300

    autocmds + layout switching

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,28 @@
+local xkbswitch = require("config.xkbswitch")
+
+local kbswitch = vim.api.nvim_create_augroup("kbswitch", { clear = true })
+vim.api.nvim_create_autocmd(xkbswitch.events_swtich_to_default, {
+	callback = xkbswitch.switch_to_default_kb,
+	group = kbswitch,
+})
+vim.api.nvim_create_autocmd(xkbswitch.events_switch_to_previous, {
+	callback = xkbswitch.switch_to_previous_kb,
+	group = kbswitch,
+})
+
+-- https://github.com/fladson/vim-kitty
+local kitty_detect = vim.api.nvim_create_augroup("kitty-detect", { clear = true })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	callback = function()
+		vim.bo.filetype = "kittyconf"
+	end,
+	pattern = "kitty.conf,*/kitty/*.conf",
+	group = kitty_detect,
+})
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	callback = function()
+		vim.bo.filetype = "kitty-session"
+	end,
+	pattern = "*/kitty/*.session",
+	group = kitty_detect,
+})
---
+++ lua/config/xkbswitch.lua
@@ -0,0 +1,72 @@
+-- Todo: make it work per buffer
+M = {
+	keyboard = "keychron-keychron-q1v1-keyboard",
+	events_swtich_to_default = {
+		"VimEnter",
+		"FocusGained",
+		"InsertLeave",
+	},
+
+	events_switch_to_previous = {
+		"InsertEnter",
+	},
+}
+
+local layouts
+local layout_names_map = {}
+local layout_idx_map = {}
+local saved_keymap
+local default_layout = "us"
+
+require("config.util")
+local get_main_device = function(obj)
+	local devices = vim.json.decode(obj.stdout)
+	for _, dev in ipairs(devices.keyboards) do
+		if dev.name == M.keyboard then
+			return dev
+		end
+	end
+end
+
+local map_layout = function(layout)
+	return function(obj)
+		layout_names_map[obj.stdout:gsub("\r?\n", ""):gsub('"', "")] = layout
+	end
+end
+
+local prepare_kb_info = function(obj)
+	local dev = get_main_device(obj)
+	layouts = Explode(",", dev.layout)
+	if type(layouts) == "boolean" then
+		layouts = { default_layout }
+	end
+	for i, l in ipairs(layouts) do
+		vim.system({
+			"bash",
+			"-c",
+			string.format('xkbcli list  | yq "[.layouts.[] | select(.layout == \\"%s\\")][0].description"', l),
+		}, { text = true }, map_layout(l))
+		layout_idx_map[l] = i - 1
+	end
+end
+
+local save_keymap = function(obj)
+	local dev = get_main_device(obj)
+	saved_keymap = layout_names_map[dev.active_keymap]
+end
+
+vim.system({ "hyprctl", "devices", "-j" }, { text = true }, prepare_kb_info)
+
+M.switch_to_default_kb = function()
+	local function switch(obj)
+		save_keymap(obj)
+		vim.system({ "hyprctl", "switchxkblayout", M.keyboard, layout_idx_map[default_layout] }, { text = true })
+	end
+	vim.system({ "hyprctl", "devices", "-j" }, { text = true }, switch)
+end
+
+M.switch_to_previous_kb = function()
+	vim.system({ "hyprctl", "switchxkblayout", M.keyboard, layout_idx_map[saved_keymap] }, { text = true })
+end
+
+return M
