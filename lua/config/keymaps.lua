d67d7b186064cbefbf09d4ff6557e775c2546522
Grigorii Matiukhin <contact@gmatiukhin.site>
2024-12-02 01:55:24 +0300

    remove buffer navigation because no bufferline

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -42,10 +42,6 @@ keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 
--- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
 -- Visual --
 -- Stay in indent mode when changing indentation of selected text
 keymap("v", "<", "<gv", opts)
