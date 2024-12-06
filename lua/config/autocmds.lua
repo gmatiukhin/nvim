c84a4d139cbaa8b181d5248df2f76393cb401b72
Grigorii Matiukhin <contact@gmatiukhin.site>
2024-12-06 21:55:35 +0300

    fix autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,18 +1,16 @@
-if os.getenv("HYPRLAND_INSTANCE_SIGNATURE") == nil then
-	return
+if os.getenv("HYPRLAND_INSTANCE_SIGNATURE") ~= nil then
+	local xkbswitch = require("config.xkbswitch")
+
+	local kbswitch = vim.api.nvim_create_augroup("kbswitch", { clear = true })
+	vim.api.nvim_create_autocmd(xkbswitch.events_swtich_to_default, {
+		callback = xkbswitch.switch_to_default_kb,
+		group = kbswitch,
+	})
+	vim.api.nvim_create_autocmd(xkbswitch.events_switch_to_previous, {
+		callback = xkbswitch.switch_to_previous_kb,
+		group = kbswitch,
+	})
 end
-
-local xkbswitch = require("config.xkbswitch")
-
-local kbswitch = vim.api.nvim_create_augroup("kbswitch", { clear = true })
-vim.api.nvim_create_autocmd(xkbswitch.events_swtich_to_default, {
-	callback = xkbswitch.switch_to_default_kb,
-	group = kbswitch,
-})
-vim.api.nvim_create_autocmd(xkbswitch.events_switch_to_previous, {
-	callback = xkbswitch.switch_to_previous_kb,
-	group = kbswitch,
-})
 
 -- https://github.com/fladson/vim-kitty
 local kitty_detect = vim.api.nvim_create_augroup("kitty-detect", { clear = true })
