eb7c57a24e35988ae6dece0ff79da49f70adb971
Grigorii Matiukhin <contact@gmatiukhin.site>
2025-01-14 01:49:28 +0300

    rust-analyzer settings

--- lua/plugins/lsp/settings/rust_analyzer.lua
+++ lua/plugins/lsp/settings/rust_analyzer.lua
@@ -1,28 +1,23 @@
 -- https://github.com/rust-lang/rust-analyzer/tree/master/docs/user#settings
 return {
-  settings = {
-    ["rust-analyzer"] = {
-      checkOnSave = true,
-      check = {
-        -- features = "all",
-        overrideCommand = {
-          "cargo",
-          "clippy",
-          "--workspace",
-          "--message-format=json",
-          "--all-targets",
-          "--all-features",
-        },
-      },
-      cargo = {
-        autoreload = true,
-        buildScripts = {
-          enable = true,
-        },
-      },
-      procMacro = {
-        enable = true,
-      },
-    },
-  },
+	settings = {
+		["rust-analyzer"] = {
+			checkOnSave = true,
+			check = {
+				command = "clippy",
+				features = "all",
+				allTargets = true,
+				workspace = false,
+			},
+			cargo = {
+				autoreload = true,
+				buildScripts = {
+					enable = true,
+				},
+			},
+			procMacro = {
+				enable = true,
+			},
+		},
+	},
 }
