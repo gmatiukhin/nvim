1950ea8b3052eaae354f8b586a9155ac8ae42931
Grigorii Matiukhin <contact@gmatiukhin.site>
2026-05-14 19:35:39 +0300

    update leap mappings

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,11 +1,12 @@
 return {
-  "ggandor/leap.nvim",
+  url = "https://codeberg.org/andyg/leap.nvim",
   dependencies = { "tpope/vim-repeat" },
   opts = {},
   config = function()
     local leap = require("leap")
     local leap_user = require("leap.user")
-    leap.create_default_mappings()
+    vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+    vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
     -- Define equivalence classes for brackets and quotes, in addition to
     -- the default whitespace group:
     leap.opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
